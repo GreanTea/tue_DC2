1e640e6519e4648abe5c4e22f6438d508375d632
DnnsvnG <d.n.v.gastel@student.tue.nl>
2023-05-09 14:21:19 +0200

    correlation between trees and burglaries in area

--- correlations.py
+++ correlations.py
@@ -6,10 +6,15 @@ df2021 = pd.read_csv('/volumes/Seagate DNvG/School/dc2/data/2021-crime-data.csv'
 df2021 = df2021[df2021['Crime type'] == 'Burglary']
 
 # for each burrow, find nr_trees in 2021 vs nr_burglaries in 2021
-nr_trees_dict = {}
+nr_trees_df = pd.DataFrame(columns=['Borough', 'nr_trees', 'burglaries'])
 for lsoa in set(df2021['LSOA name']):
     for borough in set(tree_list['borough']):
         if borough == str(lsoa)[:-5]:
-            nr_trees_dict[borough] = {'nr_trees': tree_list[tree_list['borough'] == borough].shape[0], 'burglaries':
-                                    df2021[df2021['LSOA name'].str.startswith(borough, na=False)].shape[0]}
-print(nr_trees_dict)
+            df_row = pd.Series((borough, tree_list[tree_list['borough'] == borough].shape[0],
+                                df2021[df2021['LSOA name'].str.startswith(borough, na=False)].shape[0]),
+                               index=['Borough', 'nr_trees', 'burglaries'])
+            nr_trees_df = pd.concat([nr_trees_df, pd.DataFrame([df_row])], ignore_index=True)
+nr_trees_df[['nr_trees', 'burglaries']] = nr_trees_df[['nr_trees', 'burglaries']].apply(pd.to_numeric)
+
+corr_tb = nr_trees_df['nr_trees'].corr(nr_trees_df['burglaries'])
+print(corr_tb)
