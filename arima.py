6b860b65c3585fdca2bef00f0c9692091d04450a
GreanTea <51298567+GreanTea@users.noreply.github.com>
2023-05-15 23:54:33 +0200

    Update and rename main.py to arima.py

---
+++ arima.py
@@ -0,0 +1,25 @@
+from statsmodels.tsa.arima.model import ARIMA
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv('final.csv')
+data['ds'] = pd.to_datetime(data['ds'])
+
+train_data = data[:-12]
+test_data = data[-12:]
+
+model = ARIMA(train_data['y'], order=(1, 1, 1))
+model_fit = model.fit()
+
+forecast_values = model_fit.forecast(steps=12)
+
+mae = np.mean(np.abs(test_data['y'] - forecast_values))
+
+mse = np.mean((test_data['y'] - forecast_values) ** 2)
+
+rmse = np.sqrt(mse)
+
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
